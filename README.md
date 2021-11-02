0d7c74774f57095d13f2dd4e5c736c06fc24898d
Luong Hieu Thi <luonghieuthi@gmail.com>
2021-11-02 20:41:35 +0900

    Add Search and Replace logic

--- README.md
+++ README.md
@@ -1,24 +1,23 @@
 # Rubi and Furigana
 
-This plugin adds a new command `Rubi` (can be found in `Edit` menu, Editor toolbar, or Keyboard Shortcut) that insert `<ruby>` tag into formatted text to display furigana.
-	- It only works in markdown editor. The current WYSIWYG editor of Joplin is quite problematic, so I have no use for it at the moment.
+This plugin adds a new command `Rubi` (can be found in `Edit` menu, Editor toolbar, or Keyboard Shortcut) that insert appropriate `<ruby>` and `<rt>` tags into formatted text to display furigana and vice versa.
+
+It is especially useful for Japanese (and East Asian languages in general), but it has its uses for many scenarios.
+
+It only works in markdown editor. The current WYSIWYG editor of Joplin is quite problematic, so I have no use for it at the moment.
 
 ## Usage:
 
-Select text formatted similar to the ones below:
+The current version of the plugin supports two methods for converting: Basic, and Search and Replace.
+
+### Rubi (Basic) (Shortcut: `CmdOrCtrl+R`)
+
+The `Basic` logic will add `<ruby>` tags around the seclection
 
 ```
-漢字「かんじ」
-漢「かん」字「じ」
-example(ɪɡˈzæmpl̩)
-```
-
-Activate the Rubi command by clicking on the menu item, the toolbar button, or the shortcut and turn them into something like this:
-
-```
-<ruby>漢字<rt>かんじ</rt></ruby>
-<ruby>漢<rt>かん</rt>字<rt>じ</rt></ruby>
-<ruby>example<rt>ɪɡˈzæmpl̩</rt></ruby>
+漢字「かんじ」 → <ruby>漢字<rt>かんじ</rt></ruby>
+漢「かん」字「じ」 → <ruby>漢<rt>かん</rt>字<rt>じ</rt></ruby>
+example(ɪɡˈzæmpl̩) → <ruby>example<rt>ɪɡˈzæmpl̩</rt></ruby>
 ```
 
 which are displayed as the following:
@@ -27,18 +26,32 @@ which are displayed as the following:
 <ruby>漢<rt>かん</rt>字<rt>じ</rt></ruby>
 <ruby>example<rt>ɪɡˈzæmpl̩</rt></ruby>
 
-It is especially useful for Japanese (and East Asian languages in general), but it has its uses for many scenarios.
+The `Basic` logic will only be activated when there are an equal number of open and close characters.
 
-You can also select the rubi text and toggle it back to plain text using the same command.
+### Rubi (Search and Replace) (Shortcut: `CmdOrCtrl+Shift+R`)
 
-The command is only activated when it found an equal number of open and close puntuations to reduce undesired situation.
+The `Basic` logic is fine in most case but it is inconvenient to select each term seperately if you have to insert many furigana amid a paragraph. In that case you can use `Search and Replace` logic which simply convert designated characters to approriate tags.
+
+```
+{今日(きょう)}は{漢字(かんじ)}の{書き取(かきと)}りがある 
+→ <ruby>今日<rt>きょう</rt></ruby>は<ruby>漢字<rt>かんじ</rt></ruby>の<ruby>書き取<rt>かきと</rt></ruby>りがある
+
+```
+
+which are displayed as the following:
+
+<ruby>今日<rt>きょう</rt></ruby>は<ruby>漢字<rt>かんじ</rt></ruby>の<ruby>書き取<rt>かきと</rt></ruby>りがある
+
+It is not pretty but it allows us to keep typing instead of constantly switch back and forth between keyboard and mouse to toggle ruby characters.
 
 
 ## Settings
 
-You can define which special characters will be turn into `<rt>` (Default: `([{（「『` ) and `</rt>` (Default: `)]}）」』`) in the plugin settings.
+You can define which characters will be turn into `<ruby>` (Default: `{『`), `</ruby>` (Default: `}』`), `<rt>` (Default: `([（「`), and `</rt>` (Default: `)]）」`) in the plugin settings.
 
-Shortcut (Default: `CmdOrCtrl+R`) can be changed in the shortcut editor.
+There is only one Rubi button in the editor toolbar but you can set which logic it uses in plugin settings.
+
+Keyboard Shortcuts can be changed in the shortcut editor.
 
 
 
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,8 @@ import { ToolbarButtonLocation } from 'api/types';
 import { SettingItemType } from 'api/types';
 
 
-function toggleSelectionWithRubi(selected: string|null, rubiOpens = '([{（「『', rubiCloses=')]}）」』'){
+function toggleSelectionWithRubiBasic(selected: string|null, 
+										rto = '([（「', rtc=')]）」'){
 	if (!selected) selected = '';
 
 	// Remove white space on either side of selection
@@ -24,19 +25,19 @@ function toggleSelectionWithRubi(selected: string|null, rubiOpens = '([{（「�
 		nOpens  = (ret.match(/<rt>/g) || []).length;
 		nCloses = (ret.match(/<\/rt>/g) || []).length;
 
-		ret = ret.replace(/<rt>/g, rubiOpens[0])
-		ret = ret.replace(/<\/rt>/g, rubiCloses[0])
+		ret = ret.replace(/<rt>/g, rto[0])
+		ret = ret.replace(/<\/rt>/g, rtc[0])
 	} else {
 		// Add ruby tag
 		ret = '<ruby>'
 		for (let i = 0; i < core.length; i++) {
 			const c = core[i];
-			if (rubiOpens.indexOf(c) > -1){
+			if (rto.indexOf(c) > -1){
 				ret += '<rt>';
 				nOpens += 1
 				continue;
 			}
-			if (rubiCloses.indexOf(c) > -1){
+			if (rtc.indexOf(c) > -1){
 				ret += '</rt>';
 				nCloses += 1
 				continue;
@@ -53,6 +54,44 @@ function toggleSelectionWithRubi(selected: string|null, rubiOpens = '([{（「�
 	}
 }
 
+function toggleSelectionWithRubiSnR(selected: string|null, 
+									rto = '([（「', rtc=')]）」',
+									ruo = '{『', ruc = '}』'){
+	if (!selected) selected = '';
+	if (ruo.length==0 || ruc.length==0 || rto.length==0 || rtc.length==0) {return selected;}
+
+	let ret = '';
+	if (selected.search('<ruby>')>0 || selected.search('</ruby>')>0 
+		|| selected.search('<rt>')>0 || selected.search('</rt>')>0) {
+		ret = selected.replace(/<ruby>/g, ruo[0]);
+		ret = ret.replace(/<\/ruby>/g, ruc[0]);
+		ret = ret.replace(/<rt>/g, rto[0]);
+		ret = ret.replace(/<\/rt>/g, rtc[0]);
+	} else {
+		for (let i = 0; i < selected.length; i++) {
+			const c = selected[i];
+			if (ruo.indexOf(c) > -1){
+				ret += '<ruby>';
+				continue;
+			}
+			if (ruc.indexOf(c) > -1){
+				ret += '</ruby>';
+				continue;
+			}
+			if (rto.indexOf(c) > -1){
+				ret += '<rt>';
+				continue;
+			}
+			if (rtc.indexOf(c) > -1){
+				ret += '</rt>';
+				continue;
+			}
+			ret += c;
+		}
+	}
+	return ret;
+}
+
 joplin.plugins.register({
 	onStart: async function() {
 		await joplin.settings.registerSection('settings.rubiFurigana', {
@@ -61,44 +100,97 @@ joplin.plugins.register({
 		});
 
 		await joplin.settings.registerSettings({
-			'rubi_opens': {
-				value: "([{（「『",
-				type: SettingItemType.String,
+			'rubi_default': {
+				value: 0,
+				type: SettingItemType.Int,
+				isEnum: true,
+				options: {0: "Rubi (Basic)", 1: "Rubi (Search and Replace)"},
 				section: 'settings.rubiFurigana',
 				public: true,
-				label: 'Open puntuations',
-				description: 'Characters that will be replaced with <rt>. The first puntuation will be used when convert rubi back to text.'
+				label: 'Default Rubi Command (Restart required)',
+				description: 'The default command that will be included in editor toolbar.'
 			},
-			'rubi_closes': {
-				value: ")]}）」』",
+			'rubi_rto': {
+				value: "([（「",
 				type: SettingItemType.String,
 				section: 'settings.rubiFurigana',
 				public: true,
-				label: 'Close puntuations',
-				description: 'Characters that will be replaced with </rt>. The first puntuation will be used when convert rubi back to text.'
-			}
+				label: 'Placeholders for <rt> (All commands)',
+				description: 'Characters that will be replaced with <rt>. The first character will be used when converting rubi back to text.'
+			},
+			'rubi_rtc': {
+				value: ")]）」",
+				type: SettingItemType.String,
+				section: 'settings.rubiFurigana',
+				public: true,
+				label: 'Placeholders for </rt> (All commands)',
+				description: 'Characters that will be replaced with </rt>. The first character will be used when converting rubi back to text.'
+			},
+			'rubi_ruo': {
+				value: "{『",
+				type: SettingItemType.String,
+				section: 'settings.rubiFurigana',
+				public: true,
+				label: 'Placeholders for <ruby> (Seach and Replace)',
+				description: 'Characters that will be replaced with <ruby>. The first character will be used when converting rubi back to text.'
+			},
+			'rubi_ruc': {
+				value: "}』",
+				type: SettingItemType.String,
+				section: 'settings.rubiFurigana',
+				public: true,
+				label: 'Placeholders for </ruby> (Seach and Replace)',
+				description: 'Characters that will be replaced with </ruby>. The first character will be used when converting rubi back to text.'
+			},
 		});
 
 		joplin.commands.register({
-			name : 'toggleRubi',
-			label: 'Rubi',
+			name : 'toggleRubiBasic',
+			label: 'Rubi (Basic)',
 			enabledCondition: 'markdownEditorPaneVisible',
 			iconName: 'fab fa-sketch',
 			execute: async () => {
-				const rubiOpens  = await joplin.settings.value('rubi_opens') as string;
-				const rubiCloses = await joplin.settings.value('rubi_closes') as string;
+				const rto  = await joplin.settings.value('rubi_rto') as string;
+				const rtc = await joplin.settings.value('rubi_rtc') as string;
 
 				const selectedText = await joplin.commands.execute('selectedText') as string;
 
-				const newText = toggleSelectionWithRubi(selectedText,rubiOpens,rubiCloses);
+				const newText = toggleSelectionWithRubiBasic(selectedText,rto,rtc);
 				
 				await joplin.commands.execute('editor.execCommand', {name: "replaceSelection", args: [newText, "around"]});
 				await joplin.commands.execute('editor.focus');
 			}
 		});
 
-		joplin.views.toolbarButtons.create('toggleRubiButton', 'toggleRubi', ToolbarButtonLocation.EditorToolbar);
-		joplin.views.menuItems.create('toggleRubiMenuItem', 'toggleRubi', MenuItemLocation.Edit, { accelerator: 'CmdOrCtrl+R' });
-		
+		joplin.commands.register({
+			name : 'toggleRubiSnR',
+			label: 'Rubi (Search and Replace)',
+			enabledCondition: 'markdownEditorPaneVisible',
+			iconName: 'fab fa-sketch',
+			execute: async () => {
+				const ruo = await joplin.settings.value('rubi_ruo') as string;
+				const ruc = await joplin.settings.value('rubi_ruc') as string;
+				const rto = await joplin.settings.value('rubi_rto') as string;
+				const rtc = await joplin.settings.value('rubi_rtc') as string;
+
+				const selectedText = await joplin.commands.execute('selectedText') as string;
+
+				const newText = toggleSelectionWithRubiSnR(selectedText,rto,rtc,ruo,ruc);
+				
+				await joplin.commands.execute('editor.execCommand', {name: "replaceSelection", args: [newText, "around"]});
+				await joplin.commands.execute('editor.focus');
+			}
+		});
+
+		joplin.views.menuItems.create('toggleRubiBasicMenuItem', 'toggleRubiBasic', MenuItemLocation.Edit, { accelerator: 'CmdOrCtrl+R' });
+		joplin.views.menuItems.create('toggleRubiSnRMenuItem', 'toggleRubiSnR', MenuItemLocation.Edit, { accelerator: 'CmdOrCtrl+Shift+R' });
+
+		const defcmd = await joplin.settings.value("rubi_default") as number;
+		if (defcmd==0){
+			joplin.views.toolbarButtons.create('toggleRubiButton', 'toggleRubiBasic', ToolbarButtonLocation.EditorToolbar);
+		} else {
+			joplin.views.toolbarButtons.create('toggleRubiButton', 'toggleRubiSnR', ToolbarButtonLocation.EditorToolbar);
+		}
+
 	},
 });
